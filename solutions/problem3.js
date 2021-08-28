e0e3fa3c27febd04ece59a20e79d82b4ff121c97
Ali Zakwan <alizakwanansari@gmail.com>
2021-08-29 01:22:50 +0530

    added problem3

--- solutions/problem3.js
+++ solutions/problem3.js
@@ -1,3 +1,5 @@
+// Modify the previous program such that only the users Alice and Bob are greeted with their names
+
 var readlineSync = require("readline-sync");
 
 var username = readlineSync.question("May I have your name: ");
